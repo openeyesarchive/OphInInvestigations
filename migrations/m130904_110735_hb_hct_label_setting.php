9666e45d20dcbf9937128db6dba73fe664e812dc
Mark <m@rkw.me>
2013-12-05 13:34:55 +0000

    [OE-3856] migration bugfix

--- migrations/m130904_110735_hb_hct_label_setting.php
+++ migrations/m130904_110735_hb_hct_label_setting.php
@@ -6,7 +6,10 @@ class m130904_110735_hb_hct_label_setting extends CDbMigration
 	{
 		$event_type = Yii::app()->db->createCommand()->select("id")->from("event_type")->where("class_name = :class_name",array(":class_name"=>"OphInInvestigations"))->queryRow();
 		$element_type = Yii::app()->db->createCommand()->select("id")->from("element_type")->where("event_type_id = :event_type_id and class_name = :class_name",array(":event_type_id"=>$event_type['id'],":class_name"=>"Element_OphInInvestigations_BloodTests"))->queryRow();
-		$setting_field_type = Yii::app()->db->createCommand()->select("id")->from("setting_field_type")->where("name = :name",array(":name"=>"String"))->queryRow();
+		if (!$setting_field_type = Yii::app()->db->createCommand()->select("id")->from("setting_field_type")->where("name = :name",array(":name"=>"String"))->queryRow()) {
+			$this->insert('setting_field_type',array('name' => 'String'));
+			$setting_field_type = Yii::app()->db->createCommand()->select("id")->from("setting_field_type")->where("name = :name",array(":name"=>"String"))->queryRow();
+		}
 
 		$this->insert('setting_metadata',array(
 			'element_type_id' => $element_type['id'],
