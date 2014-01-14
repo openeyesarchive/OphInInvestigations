30e83d246b72daf0efc3b38f7b818ba28d764895
Mark <m@rkw.me>
2014-01-14 09:38:48 +0000

    fixed broken db references

--- migrations/m130904_110735_hb_hct_label_setting.php
+++ migrations/m130904_110735_hb_hct_label_setting.php
@@ -4,9 +4,9 @@ class m130904_110735_hb_hct_label_setting extends CDbMigration
 {
 	public function up()
 	{
-		$event_type = Yii::app()->db->createCommand()->select("id")->from("event_type")->where("class_name = :class_name",array(":class_name"=>"OphInInvestigations"))->queryRow();
-		$element_type = Yii::app()->db->createCommand()->select("id")->from("element_type")->where("event_type_id = :event_type_id and class_name = :class_name",array(":event_type_id"=>$event_type['id'],":class_name"=>"Element_OphInInvestigations_BloodTests"))->queryRow();
-		$setting_field_type = Yii::app()->db->createCommand()->select("id")->from("setting_field_type")->where("name = :name",array(":name"=>"String"))->queryRow();
+		$event_type = $this->dbConnection->createCommand()->select("id")->from("event_type")->where("class_name = :class_name",array(":class_name"=>"OphInInvestigations"))->queryRow();
+		$element_type = $this->dbConnection->createCommand()->select("id")->from("element_type")->where("event_type_id = :event_type_id and class_name = :class_name",array(":event_type_id"=>$event_type['id'],":class_name"=>"Element_OphInInvestigations_BloodTests"))->queryRow();
+		$setting_field_type = $this->dbConnection->createCommand()->select("id")->from("setting_field_type")->where("name = :name",array(":name"=>"String"))->queryRow();
 
 		$this->insert('setting_metadata',array(
 			'element_type_id' => $element_type['id'],
@@ -19,8 +19,8 @@ class m130904_110735_hb_hct_label_setting extends CDbMigration
 
 	public function down()
 	{
-		$event_type = Yii::app()->db->createCommand()->select("id")->from("event_type")->where("class_name = :class_name",array(":class_name"=>"OphInInvestigations"))->queryRow();
-		$element_type = Yii::app()->db->createCommand()->select("id")->from("element_type")->where("event_type_id = :event_type_id and class_name = :class_name",array(":event_type_id"=>$event_type['id'],":class_name"=>"Element_OphInInvestigations_BloodTests"))->queryRow();
+		$event_type = $this->dbConnection->createCommand()->select("id")->from("event_type")->where("class_name = :class_name",array(":class_name"=>"OphInInvestigations"))->queryRow();
+		$element_type = $this->dbConnection->createCommand()->select("id")->from("element_type")->where("event_type_id = :event_type_id and class_name = :class_name",array(":event_type_id"=>$event_type['id'],":class_name"=>"Element_OphInInvestigations_BloodTests"))->queryRow();
 
 		$this->delete('setting_metadata',"element_type_id = {$element_type['id']} and `key`='hb_hct_label'");
 	}
